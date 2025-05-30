fa60bc8ab51c45ed439453620df75e0547ee88da
vicentejrmc <vicente_pc@outlook.com>
2025-05-30 09:34:56 -0300

    Modificado Modulo Equipamento

--- GestaoDeEquipamentos.ConsoleApp/ModuloEquipamento/Equipamento.cs
+++ GestaoDeEquipamentos.ConsoleApp/ModuloEquipamento/Equipamento.cs
@@ -1,28 +1,76 @@
-﻿namespace GestaoDeEquipamentos.ConsoleApp.ModuloEquipamento;
+﻿using GestaoDeEquipamentos.ConsoleApp.Compartilhado;
+using GestaoDeEquipamentos.ConsoleApp.ModuloChamado;
+using GestaoDeEquipamentos.ConsoleApp.ModuloFabricante;
 
-public class Equipamento
+namespace GestaoDeEquipamentos.ConsoleApp.ModuloEquipamento;
+
+public class Equipamento : EntidadeBase<Equipamento>
 {
-    public int Id;
-    public string Nome;
-    public string Fabricante;
-    public decimal PrecoAquisicao;
-    public DateTime DataFabricacao;
+    public string Nome { get; set; }
+    public Fabricante Fabricante { get; set; }
+    public decimal PrecoAquisicao { get; set; }
+    public DateTime DataFabricacao { get; set; }
 
-    public Equipamento(string nome, string fabricante, decimal precoAquisicao, DateTime dataFabricacao)
+    public List<Chamado> Chamados { get; set; }
+
+    public string NumeroSerie
     {
-        Nome = nome;
-        Fabricante = fabricante;
-        PrecoAquisicao = precoAquisicao;
-        DataFabricacao = dataFabricacao;
+        get
+        {
+            string tresPrimeirosCaracteres = Nome.Substring(0, 3).ToUpper();
+
+            return $"{tresPrimeirosCaracteres}-{Id}";
+        }
     }
 
-    public string Telefone { get; internal set; }
-    public string Email { get; internal set; }
-
-    public string ObterNumeroSerie()
+    public Equipamento()
     {
-        string tresPrimeirosCaracteres = Nome.Substring(0, 3).ToUpper();
-        
-        return $"{tresPrimeirosCaracteres}-{Id}";
+        Chamados = new List<Chamado>();
+    }
+
+    public Equipamento(string nome, decimal precoAquisicao, DateTime dataFabricacao, Fabricante fabricante) : this()
+    {
+        Nome = nome;
+        PrecoAquisicao = precoAquisicao;
+        DataFabricacao = dataFabricacao;
+        Fabricante = fabricante;
+    }
+
+    public void AdicionarChamado(Chamado chamado)
+    {
+        if (!Chamados.Contains(chamado))
+            Chamados.Add(chamado);
+    }
+
+    public void RemoverChamado(Chamado chamado)
+    {
+        if (Chamados.Contains(chamado))
+            Chamados.Remove(chamado);
+    }
+
+    public override void AtualizarRegistro(Equipamento equipamentoAtualizado)
+    {
+        Nome = equipamentoAtualizado.Nome;
+        DataFabricacao = equipamentoAtualizado.DataFabricacao;
+        PrecoAquisicao = equipamentoAtualizado.PrecoAquisicao;
+    }
+
+    public override string Validar()
+    {
+        string erros = "";
+
+        if (string.IsNullOrWhiteSpace(Nome))
+            erros += "O campo 'Nome' é obrigatório.\n";
+
+        if (Nome.Length < 3)
+            erros += "O campo 'Nome' precisa conter ao menos 3 caracteres.\n";
+
+        if (PrecoAquisicao <= 0)
+            erros += "O campo 'Preço de Aquisição' deve ser maior que zero.\n";
+
+        if (DataFabricacao > DateTime.Now)
+            erros += "O campo 'Data de Fabricação' deve conter uma data passada.\n";
+
+        return erros;
     }
 }
--- GestaoDeEquipamentos.ConsoleApp/ModuloEquipamento/RepositorioEquipamento.cs
+++ GestaoDeEquipamentos.ConsoleApp/ModuloEquipamento/RepositorioEquipamento.cs
@@ -3,75 +3,14 @@ using GestaoDeEquipamentos.ConsoleApp.Compartilhado;
 
 namespace GestaoDeEquipamentos.ConsoleApp.ModuloEquipamento;
 
-public class RepositorioEquipamento
+public class RepositorioEquipamento : RepositorioBaseEmArquivo<Equipamento>, IRepositorioEquipamento
 {
-    public Equipamento[] equipamentos = new Equipamento[100];
-    public int contadorEquipamentos = 0;
-
-    public Fabricante[] fabricantes = new Fabricante[100];
-
-    public void CadastrarEquipamento(Equipamento novoEquipamento)
+    public RepositorioEquipamento(ContextoDeDados contexto) : base(contexto)
     {
-        novoEquipamento.Id = GeradorIds.GerarIdEquipamento();
-
-        equipamentos[contadorEquipamentos++] = novoEquipamento;
     }
 
-    public bool EditarEquipamento(int idEquipamento, Equipamento equipamentoEditado)
+    protected override List<Equipamento> ObterRegistros()
     {
-        for (int i = 0; i < equipamentos.Length; i++)
-        {
-            if (equipamentos[i] == null) continue;
-
-            else if (equipamentos[i].Id == idEquipamento)
-            {
-                equipamentos[i].Nome = equipamentoEditado.Nome;
-                equipamentos[i].Fabricante = equipamentoEditado.Fabricante;
-                equipamentos[i].PrecoAquisicao = equipamentoEditado.PrecoAquisicao;
-                equipamentos[i].DataFabricacao = equipamentoEditado.DataFabricacao;
-
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    public bool ExcluirEquipamento(int idEquipamento)
-    {
-        for (int i = 0; i < equipamentos.Length; i++)
-        {
-            if (equipamentos[i] == null) continue;
-
-            else if (equipamentos[i].Id == idEquipamento)
-            {
-                equipamentos[i] = null;
-
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    public Equipamento[] SelecionarEquipamentos()
-    {
-        return equipamentos;
-    }
-
-    public Equipamento SelecionarEquipamentoPorId(int idEquipamento)
-    {
-        for (int i = 0; i < equipamentos.Length; i++)
-        {
-            Equipamento e = equipamentos[i];
-
-            if (e == null)
-                continue;
-
-            else if (e.Id == idEquipamento)
-                return e;
-        }
-
-        return null;
+        return contexto.Equipamentos;
     }
 }
